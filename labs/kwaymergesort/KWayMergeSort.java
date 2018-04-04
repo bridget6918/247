70e02cf3477b4d270927e7fc43468c5383b631dd
Bridg <Bridg@Kingdom-69>
2018-04-04 13:21:21 -0500

    lab 7 - late

--- labs/kwaymergesort/KWayMergeSort.java
+++ labs/kwaymergesort/KWayMergeSort.java
@@ -1,9 +1,11 @@
 package kwaymergesort;
 
+import java.util.Arrays;
+
 import timing.Ticker;
 
 public class KWayMergeSort {
-	
+
 	/**
 	 * 
 	 * @param K some positive power of 2.
@@ -13,26 +15,56 @@ public class KWayMergeSort {
 	 */
 	public static Integer[] kwaymergesort(int K, Integer[] input, Ticker ticker) {
 		int n = input.length;
-		
-		//
-		// FIXME
-		// Following just copies the input as the answer
-		//
-		// You must replace the loop below with code that performs
-		// a K-way merge sort, placing the result in ans
-		//
-		// The web page for this assignment provides more detail.
-		//
-		// Use the ticker as you normally would, to account for
-		// the operations taken to perform the K-way merge sort.
-		//
+
 		Integer[] ans = new Integer[n];
-		for (int i=0; i < n; ++i) {
-			ans[i] = input[i];
+
+		if(n <= 1) { // if n<=1, just return the input array itself - this is also the base case
+			return input;
+		}
+		else { // split by filling the two dimensional array
+			Integer[][] arrays = new Integer[K][n/K];
+			ticker.tick();
+			for(int i=0; i < n; ++i) {
+				arrays[i/(n/K)][i%(n/K)] = input[i]; // i/(n/K) = rows, % gives the column by calculating the modulus
+				ticker.tick();
+			}
+			for(int i=0; i < K; ++i) {
+				arrays[i] = kwaymergesort(K, arrays[i], ticker); // recursively call kwaymergesort to sort each array
+				ticker.tick();
+			}
+			ans = merge(arrays, ticker);
 			ticker.tick();
 		}
-		
+
 		return ans;
+	}
+
+	public static Integer[] merge(Integer[][] sorted, Ticker ticker) {
+		int row = sorted.length; 
+		int col = sorted[0].length; 
+		int[] index = new int[row]; // index for arrays to be merged
+		Integer [] output = new Integer[row*col]; // final output
+		ticker.tick(5);
+
+		for(int i=0; i < row*col; ++i) {
+			int a = Integer.MAX_VALUE; // this is the min
+			int b = 0; // will change as we sort
+			ticker.tick(2);
+			for(int j=0; j < row; ++j) {
+				if(index[j] < col) {
+					ticker.tick(2);
+					if(sorted[j][index[j]] < a) { // [j] gives the # of subarray, while [index[j]] gives the element tracked by the index array
+						a = sorted[j][index[j]];
+						b = j;
+						ticker.tick(3);
+					}
+				}
+			}
+			index[b]++;
+			output[i] = a;
+			ticker.tick(2);
+		}
+		return output;
 	}
 
 }
